2efaf65f09676744108a475498e89f196112f960
supercoderboi <ethan.paul.kakkassery@gmail.com>
2026-04-08 11:33:02 +0530

    Preload live Jarvis source in OTA editor

--- scripts/prepare-site.js
+++ scripts/prepare-site.js
@@ -3,10 +3,14 @@ const path = require("path");
 
 const rootDir = path.resolve(__dirname, "..");
 const sourceManifest = path.join(rootDir, "firmware", "manifest.json");
+const sourceSketch = path.join(rootDir, "firmware", "Jarvis", "Jarvis.ino");
 const targetDir = path.join(rootDir, "site", "firmware");
 const targetManifest = path.join(targetDir, "manifest.json");
+const targetSketch = path.join(targetDir, "Jarvis.ino");
 
 fs.mkdirSync(targetDir, { recursive: true });
 fs.copyFileSync(sourceManifest, targetManifest);
+fs.copyFileSync(sourceSketch, targetSketch);
 
 console.log(`Copied ${sourceManifest} -> ${targetManifest}`);
+console.log(`Copied ${sourceSketch} -> ${targetSketch}`);
